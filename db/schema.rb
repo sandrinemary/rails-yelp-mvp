a524aa24e163801a9a8026a9bd2120f73b86e6e7
Sandrine Mary <mary_sandrine@yahoo.de>
2024-02-23 21:20:37 +0100

    implemented Rails Yelp MVP challenge model restaurant + migration file + schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_23_183313) do
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "phone_number"
+    t.text "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
