a0c53acb86cf57e49715f8e6c236c28a14d32812
Sandrine Mary <mary_sandrine@yahoo.de>
2024-02-24 15:56:56 +0100

    implemented AddReviewToRestaurant

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_24_135532) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_24_145007) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -25,6 +25,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_24_135532) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "restaurant_id"
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
+  add_foreign_key "reviews", "restaurants"
 end
