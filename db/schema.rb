b7ba2cfce7cca1f4f7393e49c4ea91520a0d7d44
Sandrine Mary <mary_sandrine@yahoo.de>
2024-02-24 14:56:25 +0100

    implemented Rails challenge Yelp MVP review model file and db

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_23_183313) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_24_135532) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
     t.string "phone_number"
     t.text "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
